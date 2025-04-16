9b38c5e3335fa41d16513734bed06d998d3a1933
Daniel Donaldson <daniel@siyavula.com>
2025-04-16 15:55:34 +0200

    add copilot chat and remove session

---
+++ lua/custom/plugins/copilot-chat.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    keys = {
+      { '<leader>co', '<cmd>CopilotChat<cr>', desc = 'Open Copilot Chat' },
+    },
+  },
+}
